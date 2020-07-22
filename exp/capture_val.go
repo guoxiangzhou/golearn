4a7835454178f02189036eec90e5e188ccae7c62
yefu.chen <yefu.chen@zilliz.com>
2020-07-22 18:56:18 +0800

    capture test

--- exp/capture_val.go
+++ exp/capture_val.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func f1() {
 	x := 0
 	fmt.Printf("init x = %d\n", x)
 	func() {
@@ -11,4 +11,32 @@ func main() {
 		fmt.Printf("end lambda x = %d\n", x)
 	}()
 	fmt.Printf("after lambda x = %d\n", x)
+}
+
+func f2() {
+	x := 0
+	fmt.Printf("init x = %d\n", x)
+	func(x interface{}) {
+		fmt.Printf("begin lambda x = %d\n", x)
+		x = 1
+		fmt.Printf("end lambda x = %d\n", x)
+	}(x)
+	fmt.Printf("after lambda x = %d\n", x)
+}
+
+//func f3() {
+//	x := 0
+//	fmt.Printf("init x = %d\n", x)
+//	func(x interface{}) {
+//		fmt.Printf("begin lambda x = %d\n", *x)
+//		x = 1
+//		fmt.Printf("end lambda x = %d\n", *x)
+//	}(&x)
+//	fmt.Printf("after lambda x = %d\n", x)
+//}
+
+func main() {
+	f1()
+	f2()
+	//f3()
 }
