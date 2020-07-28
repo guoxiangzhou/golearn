720a93d839f8a14d2b7202100210ad2965896901
yefu.chen <yefu.chen@zilliz.com>
2020-07-28 19:40:59 +0800

    health

--- exp/etcd_client_exp.go
+++ exp/etcd_client_exp.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"go.etcd.io/etcd/clientv3"
-	gcode "google.golang.org/grpc/codes"
 	"google.golang.org/grpc/grpclog"
-	gs "google.golang.org/grpc/status"
 	"log"
 	"os"
 	"time"
@@ -14,8 +11,9 @@ import (
 
 func main() {
 	clientv3.SetLogger(grpclog.NewLoggerV2(os.Stderr, os.Stderr, os.Stderr))
+	endpoints := []string{"127.0.0.1:10001", "127.0.0.1:10002", "127.0.0.1:10003"}
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"127.0.0.1:10001", "127.0.0.1:10002", "127.0.0.1:10003"},
+		Endpoints:   endpoints,
 		DialTimeout: 1 * time.Second,
 	})
 	if err != nil {
@@ -23,32 +21,41 @@ func main() {
 	}
 	defer cli.Close()
 
-	//if _, err = cli.Put(context.TODO(), "foo", "bar"); err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//if rsp, err := cli.Get(context.TODO(), "foo"); err == nil {
-	//	for _, kv := range rsp.Kvs {
-	//		log.Printf("key=%s,value=%s, version=%d\n", kv.Key, kv.Value, kv.Version)
-	//	}
-	//} else {
-	//	log.Fatal(err)
-	//}
-
-	for i := 0; i < 100000; i++ {
-		key := fmt.Sprint("key%d", i)
-		value := fmt.Sprint("value%d", i)
-		_, err = cli.Put(context.TODO(), key, value)
-		fmt.Print(".")
-		for err != nil {
-			grpcErr, _ := gs.FromError(err)
-			if grpcErr.Code() == gcode.InvalidArgument {
-				log.Fatalf("falt error, index = %d, error = %v\n", i, err)
-			}
-			log.Printf("==================================\n")
-			log.Printf("grpc error, code = %v \n", grpcErr.Code())
-			_, err = cli.Put(context.TODO(), key, value)
-			fmt.Print(".")
+	for _, ep := range endpoints {
+		cfg := clientv3.Config{
+			Endpoints:            []string{ep},
+			DialTimeout:          2 * time.Second,
+			DialKeepAliveTime:    2 * time.Second,
+			DialKeepAliveTimeout: 6 * time.Second,
 		}
+		log.Println(cfg)
+		localCli, err := clientv3.New(cfg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ctx, cancel := context.WithTimeout(context.TODO(), 1*time.Second)
+		rsp, err := localCli.Get(ctx, "health")
+		cancel()
+		log.Println("===============")
+		log.Println(rsp)
+		log.Println(err)
 	}
+
+	//for i := 0; i < 100000; i++ {
+	//	key := fmt.Sprint("key%d", i)
+	//	value := fmt.Sprint("value%d", i)
+	//	_, err = cli.Put(context.TODO(), key, value)
+	//	fmt.Print(".")
+	//	for err != nil {
+	//		log.Println("=========================================================")
+	//		for _, ep := range endpoints {
+	//			resp, err := cli.Status(context.TODO(), ep)
+	//			if err == nil {
+	//				log.Println("=======================================================")
+	//				log.Println(resp)
+	//			}
+	//		}
+	//		return
+	//	}
+	//}
 }
