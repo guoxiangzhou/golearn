1ccf1feb48272097ee20d3ae817b1ae36e9e6712
yefu.chen <yefu.chen@zilliz.com>
2020-08-14 11:15:19 +0800

    raftexp3

--- raftexp3/main.go
+++ raftexp3/main.go
@@ -3,21 +3,32 @@ package main
 import (
 	"bytes"
 	"context"
+	"database/sql"
 	"encoding/gob"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"go.etcd.io/etcd/pkg/pbutil"
 	"go.etcd.io/etcd/raft"
 	"go.etcd.io/etcd/raft/raftpb"
 	"log"
+	"os"
 	"sync"
 	"time"
+)
+
+const (
+	walPath = "/tmp/wal.db"
 )
 
 type node struct {
 	raft.Node
 	storage *raft.MemoryStorage
 
-	mu      sync.Mutex
+	kvMu    sync.Mutex
 	kvstore map[string]string
+
+	walMu sync.Mutex
+	wal   *sql.DB
 }
 
 type kv struct {
@@ -42,9 +53,9 @@ func (n *node) applyCommits(entries []raftpb.Entry) {
 				if err := dec.Decode(&datakv); err != nil {
 					log.Fatal(err)
 				}
-				n.mu.Lock()
+				n.kvMu.Lock()
 				n.kvstore[datakv.Key] = datakv.Value
-				n.mu.Unlock()
+				n.kvMu.Unlock()
 			}
 		}
 	}
@@ -56,6 +67,41 @@ func (n *node) propose(key string, value string) {
 		log.Fatal(err)
 	}
 	n.Propose(context.TODO(), buf.Bytes())
+}
+
+func (n *node) createWalTables() {
+	if _, err := n.wal.Exec("CREATE TABLE wal (idx INTEGER ,data BLOB)"); err != nil {
+		log.Fatal(err)
+	}
+	// idx = 0 for hardstate
+	if _, err := n.wal.Exec("INSERT INTO wal VALUES (0,NULL)"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (n *node) saveWal(st raftpb.HardState, ents []raftpb.Entry) {
+	n.walMu.Lock()
+	defer n.walMu.Unlock()
+
+	if !raft.IsEmptyHardState(st) {
+		stmt, err := n.wal.Prepare("UPDATE wal SET data = ? WHERE idx=0")
+		if err != nil {
+			log.Fatal(err)
+		}
+		stmt.Exec(pbutil.MustMarshal(&st))
+		stmt.Close()
+	}
+	if len(ents) > 0 {
+		stmt, err := n.wal.Prepare("INSERT INTO wal VALUES (?,?)")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, ent := range ents {
+			stmt.Exec(ent.Index, pbutil.MustMarshal(&ent))
+		}
+		stmt.Close()
+	}
+
 }
 
 func main() {
@@ -76,6 +122,19 @@ func main() {
 		kvstore: make(map[string]string),
 	}
 
+	if _, err := os.Stat(walPath); err == nil {
+		os.Remove(walPath)
+	}
+
+	db, err := sql.Open("sqlite3", walPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	n.wal = db
+	defer n.wal.Close()
+
+	n.createWalTables()
+
 	stopc := make(chan struct{})
 
 	go func() {
@@ -85,6 +144,8 @@ func main() {
 			case <-ticker:
 				n.Tick()
 			case rd := <-n.Ready():
+				n.saveWal(rd.HardState, rd.Entries)
+
 				if !raft.IsEmptyHardState(rd.HardState) {
 					n.storage.SetHardState(rd.HardState)
 				}
